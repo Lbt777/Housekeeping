0ef2987d5d214d5f4209ef7bae23d153c33a4e96
Lbt777 <46510415+Lbt777@users.noreply.github.com>
2019-05-15 15:45:30 +0800

    首页banner模块

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import App from './App'
 import router from './router'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-Vue.config.productionTip = false
 Vue.use(Element)
 /* eslint-disable no-new */
 new Vue({
