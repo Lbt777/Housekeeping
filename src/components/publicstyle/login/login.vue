8adc071e3710b0920fa3ac2eb4e8aa82928b247c
Lbt777 <46510415+Lbt777@users.noreply.github.com>
2019-05-21 11:18:16 +0800

    登录验证

--- src/components/publicstyle/login/login.vue
+++ src/components/publicstyle/login/login.vue
@@ -1,30 +1,83 @@
 <template>
     <div class="over">
-        <form class="login">
-            <div class="font-16 viptxt">会员登录</div>
-            <el-input placeholder="请输入账号" v-model="input1" clearable></el-input>
-            <el-input placeholder="请输入密码" v-model="input2" clearable></el-input>
-            <slide-verify :l="42" :r="10" :w="310" :h="155" @success="onSuccess" @fail="onFail" @refresh="onRefresh" :slider-text="text"></slide-verify>
-            <div class="msg">
-                <div>{{msg}}</div>
-            </div>
-            <el-button type="primary">登录</el-button>
-            <router-link to="/register">免费注册</router-link>
-        </form>
+      <div class="login">
+        <div class="font-16 viptxt">会员登录</div>
+        <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+            <el-form-item label="手机" prop="phone">
+            <el-input v-model="ruleForm.phone" placeholder="手机号登录"></el-input>
+          </el-form-item>
+          <el-form-item label="密码"  prop="pass">
+            <el-input type="password" v-model="ruleForm.pass" autocomplete="off" placeholder="密码"></el-input>
+          </el-form-item>
+          <slide-verify :l="42" :r="10" :w="310" :h="155" @success="onSuccess" @fail="onFail" @refresh="onRefresh" :slider-text="text"></slide-verify>
+          <div class="msg">
+              <div>{{msg}}</div>
+          </div>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+          </el-form-item>
+        </el-form>
+         <router-link to="/register">免费注册</router-link>
+      </div>
     </div>
 </template>
 <script>
 export default {
   data () {
+    var phone = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('账号错误'))
+      } else {
+        var reg = /^1[34578]\d{9}$/
+        if (reg.test(value) === false) {
+          return callback(new Error('请输入正确的账号'))
+        } else {
+          callback()
+        }
+      }
+    }
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        if (this.ruleForm.checkPass !== '') {
+          this.$refs.ruleForm.validateField('checkPass')
+        }
+        callback()
+      }
+    }
     return {
-      input1: '',
-      input2: '',
       msg: '',
       text: '向右滑',
-      input: ''
+      ruleForm: {
+        pass: '',
+        phone: ''
+      },
+      activeName: 'second',
+      rules: {
+        pass: [
+          { validator: validatePass, trigger: 'blur' }
+        ],
+        phone: [
+          { validator: phone, trigger: 'blur' }
+        ]
+      }
     }
   },
   methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    },
     onSuccess () {
       this.msg = '验证成功'
     },
